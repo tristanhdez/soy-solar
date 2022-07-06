a03da57f6b78d6171106c63ee8bdbf42c9475f03
Tristan <tristanhdez01@gmail.com>
2022-07-06 17:18:20 -0500

    Added development environment

--- config.py
+++ config.py
@@ -6,16 +6,24 @@ from dotenv import load_dotenv
 basedir = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(basedir, '.env'))
 
+
 class Config:
     """BASE CONFIG"""
     SECRET_KEY = os.environ.get('SECRET_KEY')
     STATIC_FOLDER = 'static'
     TEMPLATES_FOLDER = 'templates'
-    MYSQL_DATABASE_HOST = ''
-    MYSQL_DATABASE_USER = ''
-    MYSQL_DATABASE_PASSWORD = ''
-    MYSQL_DATABASE_DB= ''
-    #CSRF_ENABLED = False
+    MYSQL_DATABASE_HOST = 'localhost'
+    MYSQL_DATABASE_USER = 'root'
+    MYSQL_DATABASE_PASSWORD = 'Password123*'
+    MYSQL_DATABASE_DB= 'solar'
+    
+    MAIL_SERVER='smtp.gmail.com'
+    MAIL_PORT= 465
+    MAIL_USERNAME = 'solarbotsupp@gmail.com'
+    MAIL_PASSWORD = 'rljfpaheyxwmroop'
+    #MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    #CSRF_ENABLED = True
     #SESSION_COOKIE_SECURE=True
     #SESSION_COOKIE_HTTPONLY=True
     #SESSION_COOKIE_SAMESITE='Lax'
