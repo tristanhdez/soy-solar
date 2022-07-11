81410cb230ec675d1ba43aab7de7b2995d66ab4c
Tristan <tristanhdez01@gmail.com>
2022-07-11 18:32:21 -0500

    Added Home Blueprint

---
+++ apps/home/home.py
@@ -0,0 +1,53 @@
+from flask import Blueprint, render_template
+
+
+home = Blueprint("home",__name__, static_folder="static", template_folder="templates")
+
+@home.route("/")
+def index():
+    return render_template('home/index.html')
+
+
+@home.route("/section")
+def section():
+    return render_template('home/section.html')
+
+
+@home.errorhandler(400)
+def handle_bad_request(e):
+    return render_template('errors/client/400.html'), 400
+
+
+@home.errorhandler(403)
+def handle_bad_request(e):
+    return render_template('errors/client/400.html'), 403
+
+
+@home.errorhandler(404)
+def not_found(self):
+    return render_template('errors/client/404.html'), 404
+
+
+@home.errorhandler(405)
+def method_not_found(error):
+    return render_template('errors/client/405.html'), 405
+
+
+@home.errorhandler(408)
+def request_timeout(error):
+    return render_template('errors/client/408.html'), 408
+
+
+@home.errorhandler(410)
+def page_gone(error):
+    return render_template('errors/client/410.html'), 410
+
+
+@home.errorhandler(500)
+def internal_error(error):
+    return render_template('errors/server/500.html'), 500
+
+
+@home.errorhandler(505)
+def http_not_compatible(error):
+    return render_template('errors/server/500.html'), 505
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, redirect, render_template, request, url_for
-from apps.ingc.ingc import ingc
+from flask import Flask
+from apps.student.student import student
 from apps.guest.guest import guest
+from apps.home.home import home
 from config import configs
 from apps.utils.database import *
 import os
@@ -8,62 +9,17 @@ import os
 
 
 app = Flask(__name__)
-app.register_blueprint(ingc, url_prefix="/ingc")
+app.register_blueprint(student, url_prefix="/student")
 app.register_blueprint(guest, url_prefix="/guest")
+app.register_blueprint(home, url_prefix="/")
 app.config.from_object(configs[os.environ.get("FLASK_ENV", "development")])
 mysql.init_app(app)
 #csrf = CSRFProtect()
 #csrf.init_app(app)
 
 
-@app.route("/")
-def home():
-    return render_template('index.html')
-
-
-@app.route("/section")
-def section():
-    return render_template('section.html')
-
-
-@app.errorhandler(400)
-def handle_bad_request(e):
-    return render_template('errors/client/400.html'), 400
-
-
-@app.errorhandler(403)
-def handle_bad_request(e):
-    return render_template('errors/client/400.html'), 403
-
-
-@app.errorhandler(404)
-def not_found(self):
-    return render_template('errors/client/404.html'), 404
-
-
-@app.errorhandler(405)
-def method_not_found(error):
-    return render_template('errors/client/405.html'), 405
-
-
-@app.errorhandler(408)
-def request_timeout(error):
-    return render_template('errors/client/408.html'), 408
-
-
-@app.errorhandler(410)
-def page_gone(error):
-    return render_template('errors/client/410.html'), 410
-
-
-@app.errorhandler(500)
-def internal_error(error):
-    return render_template('errors/server/500.html'), 500
-
-@app.errorhandler(505)
-def http_not_compatible(error):
-    return render_template('errors/server/500.html'), 505
-
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    try:
+        app.run(host='0.0.0.0', port=5000, debug=True)
+    except Exception as e:
+        print(str(e))
