bc33b1c09ce36fd12731f9e1a97db6c89442706b
Tristan <tristanhdez01@gmail.com>
2022-08-03 17:27:39 -0500

    Fixed Deprecated of FLASK_ENV and changed by FLASK_DEBUG

--- config.py
+++ config.py
@@ -32,7 +32,6 @@ class StagingConfig(Config):
 
 
 #export FLASK_DEBUG=development
-#FLASK_ENV' is deprecated and will not be used in Flask 2.3. Use 'FLASK_DEBUG' instead
 class DevConfig(Config):
     FLASK_DEBUG = 'development'
     DEBUG = True
