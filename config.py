506f5622d665e81e5e02b031fe4e60e3c334d4b8
Tristan <tristanhdez01@gmail.com>
2022-08-04 17:55:31 -0500

    Added Cookies Security and Session on Default Config

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from os import environ, path
 import os
 from dotenv import load_dotenv
-
+from datetime import timedelta
 
 basedir = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(basedir, '.env'))
@@ -9,10 +9,15 @@ load_dotenv(path.join(basedir, '.env'))
 
 class Config:
     """BASE CONFIG"""
+    SESSION_COOKIE_SECURE=True
+    PERMANENT_SESSION_LIFETIME=timedelta(minutes=120)
+    SESSION_COOKIE_HTTPONLY=True
+    SESSION_COOKIE_SAMESITE='Lax'
+    REMEMBER_COOKIE_SECURE = True
     SECRET_KEY = os.environ.get('SECRET_KEY')
     STATIC_FOLDER = 'static'
     TEMPLATES_FOLDER = 'templates'
-    MYSQL_DATABASE_HOST = 'localhost'
+    MYSQL_DATABASE_HOST = ''
     MYSQL_DATABASE_USER = ''
     MYSQL_DATABASE_PASSWORD = ''
     MYSQL_DATABASE_DB= ''
