b5b1cd8fd1a83045c8757f5bf48e6b40b8e47cef
Tristan <tristanhdez01@gmail.com>
2022-08-29 21:28:27 -0500

    Added pyfile

--- main.py
+++ main.py
@@ -5,21 +5,26 @@ from apps.home.home import home
 from config import configs
 from apps.utils.database import *
 import os
-#from flask_wtf.csrf import CSRFProtect
 
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
+
+
 app.register_blueprint(student, url_prefix="/student")
 app.register_blueprint(guest, url_prefix="/guest")
 app.register_blueprint(home, url_prefix="/")
+
+
 app.config.from_object(configs[os.environ.get("FLASK_DEBUG", "development")])
+app.config.from_pyfile('config.py', silent=True)
+
+
 mysql.init_app(app)
-#csrf = CSRFProtect()
-#csrf.init_app(app)
+
 
 
 if __name__ == "__main__":
     try:
-        app.run(host='0.0.0.0', port=5000, debug=False)
+        app.run(host='localhost', port=5000, debug=True)
     except Exception as e:
         print(str(e))
